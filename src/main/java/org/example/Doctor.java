a7318d65e6380b043f69911aee3fe8ed9ef5a84d
mehoncevmv <mk.mekhotsev@gmail.com>
2025-09-11 14:22:21 +0300

    0.2

--- src/main/java/org/example/Doctor.java
+++ src/main/java/org/example/Doctor.java
@@ -1,15 +1,34 @@
 package org.example;
 
 public class Doctor {
-    public String name, surname;
-    public String post;
-    public Doctor(String name, String surname, String post){
+    public String name, surname, post;
+    public int age;
+    public Doctor(String name, String surname, String post, int age){//конструктор
         this.name = name;
         this.surname = surname;
         this.post = post;
+        this.age = age;
     }
 
-    public void ShowName(){
-        System.out.printf(name, surname);
+    public void showName(){//показ имени
+        System.out.println(name);
+        System.out.println(surname);
+    }
+
+    public void showPost(){//показ должности
+        System.out.println(post);
+    }
+
+    public void changePost(String newPost){//смена должности
+        this.post = newPost;
+    }
+
+    public void getOld(){//старение доктора
+        this.age++;
+    }
+    public void dismiss(){//увольнение
+        if(this.post != null){
+            this.post = null;
+        }
     }
 }
--- src/main/java/org/example/Patient.java
+++ src/main/java/org/example/Patient.java
@@ -1,4 +1,27 @@
 package org.example;
 
 public class Patient {
+    public String name, surname, disease;
+    public int age;
+    public Doctor doctor;
+    public Patient(String name, String surname, String disease, int age, Doctor doctor){
+        this.name = name;
+        this.surname = surname;
+        this.disease = disease;
+        this.age = age;
+        this.doctor = doctor;
+    }
+
+    public void changeDoctor(Doctor doctor){//смена доктора
+        this.doctor = doctor;
+    }
+
+    public void getOld(){//старение пациента
+        this.age++;
+    }
+
+    public void recovered(){//выписка(выздоровление)
+        this.doctor = null;
+        this.disease = null;
+    }
 }
