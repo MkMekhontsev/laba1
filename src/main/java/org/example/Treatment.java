4bada2ebf71d90c0a3249741486a01dc11c83754
MkMekhontsev <mkmekhontsev@gmail.com>
2025-09-24 16:12:34 +0300

    0.2

--- src/main/java/org/example/Treatment.java
+++ src/main/java/org/example/Treatment.java
@@ -1,4 +1,17 @@
 package org.example;
 
 public class Treatment {
+    public Doctor doctor;
+    public Patient patient;
+    public String disease, pills;
+    public Treatment(Doctor doctor, Patient patient, String pills){
+        this.doctor = doctor;
+        this.patient = patient;
+        this.disease = patient.disease;
+        this.pills = pills;
+    }
+
+    public void showTreatment(){
+        System.out.println(pills);
+    }
 }
